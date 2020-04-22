8e2c44dc05fe2fcb25a302be661069a405f0e906
Jake <jworsham9697@gmail.com>
2020-04-22 17:08:44 -0500

    first commit for Lab 4

---
+++ Lab-4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            bool runAgain = true;
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter an integer: ");
+                int total = int.Parse(Console.ReadLine());
+                {
+
+                    for (i = 1; i <= total; i++)
+
+                    {
+                        Console.WriteLine(i * i);
+                    }
+
+                    for (i = 1; i <= total; i++)
+
+                    {
+                        Console.WriteLine(i * i * i);
+                    }
+                    Console.WriteLine("would you like to continue? y/n? ");
+                    string yn = Console.ReadLine();
+                    if (yn == "y")
+                    {
+                        runAgain = true;
+                    }                   
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
